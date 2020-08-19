ea0b27c5261a71fd743f79d14db55daad555d964
Ryan Dung Le <ryandungl3@gmail.com>
2020-08-19 02:53:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function shout(string){
+  return string.toUpperCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(stirng.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase()){
+    return "I can't hear you!";
+  }
+}
